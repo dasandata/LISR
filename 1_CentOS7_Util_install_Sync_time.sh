f306c9584500e5ec04ae68aa0ebd8b7606d2eaac
dasandata <dasandata@gmail.com>
2017-08-18 20:31:53 +0900

    Update 1_CentOS7_Util_install_Sync_time.sh

--- 1_CentOS7_Util_install_Sync_time.sh
+++ 1_CentOS7_Util_install_Sync_time.sh
@@ -1,12 +1,13 @@
 #CentOS7 
-echo "##### yum update"
 echo " " 
 echo "##### yum install to default utils" 
+echo " " 
 yum install -y \
 vim pciutils perl openssh mlocate nfs-utils rdate xauth firefox nautilus wget htop tcsh tree lshw mailx tmux \
 git
 echo " " 
 echo "##### time sync" 
+echo " " 
 rdate  -s  time.bora.net 
 clock --systohc  
 date
