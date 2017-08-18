0f935d9152d6f8b37967b795bdcf8cef2e8a68bf
dasandata <dasandata@gmail.com>
2017-08-18 20:29:33 +0900

    yum update remove

--- 1_CentOS7_Util_install_Sync_time.sh
+++ 1_CentOS7_Util_install_Sync_time.sh
@@ -1,13 +1,12 @@
 #CentOS7 
-echo "yum update"
-yum update
+echo "##### yum update"
 echo " " 
-echo "yum install to default utils" 
+echo "##### yum install to default utils" 
 yum install -y \
 vim pciutils perl openssh mlocate nfs-utils rdate xauth firefox nautilus wget htop tcsh tree lshw mailx tmux \
 git
 echo " " 
-echo "time sync" 
+echo "##### time sync" 
 rdate  -s  time.bora.net 
 clock --systohc  
 date
