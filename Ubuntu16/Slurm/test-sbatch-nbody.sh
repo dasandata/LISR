4e62321968654c5f717c9f17b06b21f39c2cc10b
cheekykite <xxue123@gmail.com>
2018-04-03 18:19:42 +0900

    no message

--- Ubuntu16/Slurm/test-sbatch-nbody.sh
+++ Ubuntu16/Slurm/test-sbatch-nbody.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#SBATCH -J  nbody             # Job name
-#SBATCH -o  test-nbody.%j     # Name of stdout output file (%j expands to %jobId)
-#SBATCH -p  gpu               # queue or partiton name
-#SBATCH -t  01:30:00          # Max Run time (hh:mm:ss) - 1.5 hours
+#SBATCH -J  nbody                   # Job name
+#SBATCH -o  out.test-nbody.%j       # Name of stdout output file (%j expands to %jobId)
+#SBATCH -p  gpu                     # queue or partiton name
+#SBATCH -t  01:30:00                # Max Run time (hh:mm:ss) - 1.5 hours
 
-#SBATCH --gres=gpu:1          # Num Devices
+#SBATCH --gres=gpu:1                # Num Devices
 
 /root/NVIDIA_CUDA-8.0_Samples/bin/x86_64/linux/release/nbody -benchmark -numbodies=1024000 -numdevices=1
 
--- Ubuntu16/Slurm/test-sbatch-tensorflow-2GPU.sh
+++ Ubuntu16/Slurm/test-sbatch-tensorflow-2GPU.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#SBATCH -J  nbody             # Job name
-#SBATCH -o  test-nbody.%j     # Name of stdout output file (%j expands to %jobId)
-#SBATCH -p  gpu               # queue or partiton name
-#SBATCH -t  01:30:00          # Max Run time (hh:mm:ss) - 1.5 hours
+#SBATCH -J  nbody                         # Job name
+#SBATCH -o  out.test-tensorflow-2GPU.%j   # Name of stdout output file (%j expands to %jobId)
+#SBATCH -p  gpu                           # queue or partiton name
+#SBATCH -t  01:30:00                      # Max Run time (hh:mm:ss) - 1.5 hours
 
-#SBATCH --gres=gpu:2          # Num Devices
+#SBATCH --gres=gpu:2                      # Num Devices
 
 python3   /root/TensorFlow-Examples/examples/5_DataManagement/tensorflow_dataset_api.py
 
