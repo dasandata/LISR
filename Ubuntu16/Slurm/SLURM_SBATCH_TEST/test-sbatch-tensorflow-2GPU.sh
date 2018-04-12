11b650a6c8ce2e6f577a14de229a8718712a9116
cheekykite <xxue123@gmail.com>
2018-04-12 10:51:11 +0900

    1. 셔뱅 수정 (#!/bin/sh  ==>> #!/bin/bash) 2. Sample 파일 경로 변경 (/root/ ==>> ~/)

--- Ubuntu16/Slurm/SLURM_SBATCH_TEST/test-sbatch-tensorflow-2GPU.sh
+++ Ubuntu16/Slurm/SLURM_SBATCH_TEST/test-sbatch-tensorflow-2GPU.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-#SBATCH -J  nbody                         # Job name
+#SBATCH -J  tensor-2GPU                   # Job name
 #SBATCH -o  out.test-tensorflow-2GPU.%j   # Name of stdout output file (%j expands to %jobId)
 #SBATCH -p  gpu                           # queue or partiton name
 #SBATCH -t  01:30:00                      # Max Run time (hh:mm:ss) - 1.5 hours
 
 #SBATCH --gres=gpu:2                      # Num Devices
 
-python3   /root/TensorFlow-Examples/examples/5_DataManagement/tensorflow_dataset_api.py
+python3   ~/TensorFlow-Examples/examples/5_DataManagement/tensorflow_dataset_api.py
 
 # End of File.
