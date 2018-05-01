d9e674371cfb261a327be53c1bc91afc9d600dbc
cheekykite <xxue123@gmail.com>
2018-05-01 17:25:39 +0900

    no message

--- common/usr-local-sbin/dasan_temperature_log_to_mail.sh
+++ common/usr-local-sbin/dasan_temperature_log_to_mail.sh
@@ -20,9 +20,9 @@ echo  "
 # This Message from ${TITLE_TAIL}
 # Using crontab (/etc/crontab)
 # The location of the configuration file is below.
-# /root/dasan_export_global_variable.sh,
-# /root/dasan_temperature_check_to_log.sh,
-# /root/dasan_temperature_log_to_mail.sh
+# /usr/local/sbin/dasan_export_global_variable.sh,
+# /usr/local/sbin/dasan_temperature_check_to_log.sh,
+# /usr/local/sbin/dasan_temperature_log_to_mail.sh
 ##################################################
 " >> ${LOG}
 
