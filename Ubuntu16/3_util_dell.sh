251f80b5e69e6f48ea67a049986e0517eab68d94
milli-sewoo <pro3ax@gmail.com>
2018-10-23 19:52:13 +0900

    no message

---
+++ Ubuntu16/3_util_dell.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+PROMPT="[${USER}@${HOSTNAME%%.*} ${PWD/#$HOME/~}]#"
+
+echo " # 기본 유틸 설치. "
+echo -e  "\033[1;34m${PROMPT} apt-get install -y vim nfs-common rdate xauth firefox gcc make \033[0m"
+echo -e  "\033[1;34m${PROMPT} apt-get install -y locate htop tmux git wget ipmitool \033[0m"
+echo -e  "\033[1;34m${PROMPT} apt-get install -y xfsprogs ntfs-3g aptitude lvm2 dstat curl  \033[0m"
+apt-get install -y  \
+vim nfs-common rdate xauth firefox gcc make locate htop tmux git wget ipmitool \
+xfsprogs ntfs-3g aptitude lvm2 dstat curl
+
+echo ""
+echo ""
+
+COUNTER=10
+
+until [  $COUNTER -lt 1 ]; do
+
+      echo COUNTER $COUNTER
+
+      let COUNTER-=1
+
+done
+
+sleep 10
+
+echo -e  "\033[1;34m${PROMPT} apt-get install -y python-dev \033[0m"
+apt-get install -y python-dev
+
+echo ""
+echo ""
+
+echo " # 서버 시간 동기화. "
+echo -e  "\033[1;34m${PROMPT} rdate  -s  time.bora.net \033[0m"
+echo -e  "\033[1;34m${PROMPT} hwclock --systohc \033[0m"
+echo -e  "\033[1;34m${PROMPT} date \033[0m"
+echo -e  "\033[1;34m${PROMPT} hwclock \033[0m"
+rdate  -s  time.bora.net
+hwclock --systohc
+date
+hwclock
