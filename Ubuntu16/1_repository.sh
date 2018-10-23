0ebbe48776658a088525a75723969a3bd5a58cb7
milli-sewoo <pro3ax@gmail.com>
2018-10-23 18:57:03 +0900

    no message

--- Ubuntu16/1_repository.sh
+++ Ubuntu16/1_repository.sh
@@ -6,12 +6,17 @@ echo -e  "\033[1;34m${PROMPT} cat /etc/apt/sources.list | grep -v \"#\|^$\" \033
 cat /etc/apt/sources.list  |  grep -v "#\|^$"
 echo " # 기본 저장소 확인 "
 
+echo ""
+echo ""
+echo ""
 
-
-
-
-echo " # Security 부분 변경 "
+echo -e  "\033[1;34m${PROMPT} perl -pi -e 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list \033[0m"
 perl -pi -e 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+echo " # Security 부분 변경 "
+
+echo ""
+echo ""
+echo ""
 
 echo " # 변경 내용 확인 "
 cat /etc/apt/sources.list  |  grep -v "#\|^$"
