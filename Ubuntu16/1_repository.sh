4edc82dc4c65647ddbf19b24926911f36b05e5b6
milli-sewoo <pro3ax@gmail.com>
2018-10-23 18:08:09 +0900

    no message

---
+++ Ubuntu16/1_repository.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PROMPT="[${USER}@${HOSTNAME%%.*} ${PWD/#$HOME/~}]#"
+
+echo -e  "\033[1;34m${PROMPT} dmidecode --type system | grep -v \"^$\|#\|SMBIOS\|Handle\|Not\" \033[0m"
+dmidecode --type system | grep -v "^$\|#\|SMBIOS\|Handle\|Not"
+
+echo " # 기본 저장소 확인 "
+cat /etc/apt/sources.list  |  grep -v "#\|^$"
+
+echo " # Security 부분 변경 "
+perl -pi -e 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+
+echo " # 변경 내용 확인 "
+cat /etc/apt/sources.list  |  grep -v "#\|^$"
+
+echo " # 적용 하기 "
+apt-get update
