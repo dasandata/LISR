e00d50a57132f055e726ca35c72b62038cfeef7e
milli-sewoo <pro3ax@gmail.com>
2018-10-23 18:50:26 +0900

    no message

--- Ubuntu16/1_repository.sh
+++ Ubuntu16/1_repository.sh
@@ -2,11 +2,13 @@
 
 PROMPT="[${USER}@${HOSTNAME%%.*} ${PWD/#$HOME/~}]#"
 
-echo -e  "\033[1;34m${PROMPT} cat /etc/apt/sources.list | grep -v \"^$\|#\|SMBIOS\|Handle\|Not\" \033[0m"
-cat /etc/apt/sources.list | grep -v "^$\|#\|SMBIOS\|Handle\|Not"
-
-echo " # 기본 저장소 확인 "
+echo -e  "\033[1;34m${PROMPT} cat /etc/apt/sources.list | grep -v \"#\|^$\" \033[0m"
 cat /etc/apt/sources.list  |  grep -v "#\|^$"
+echo " # 기본 저장소 확인 "
+
+
+
+
 
 echo " # Security 부분 변경 "
 perl -pi -e 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
