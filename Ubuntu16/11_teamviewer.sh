93b30a58cd813c2401e8db21bf6384f1024610ee
milli-sewoo <pro3ax@gmail.com>
2018-10-24 18:19:56 +0900

    no message

--- Ubuntu16/11_teamviewer.sh
+++ Ubuntu16/11_teamviewer.sh
@@ -16,7 +16,7 @@ echo ""
 echo " Teamviewer Passwd 설정 "
 echo ""
 echo -e  "\033[1;34m${PROMPT} teamviewer passwd ******* \033[0m"
-read teamviewer passwd
+teamviewer passwd
 
 echo ""
 echo " Teamviewer 실행 및 id 확인 "
@@ -24,11 +24,39 @@ echo ""
 echo -e  "\033[1;34m${PROMPT} teamviewer daemon enable \033[0m"
 teamviewer daemon enable
 echo ""
+
+COUNTER=5
+
+until [  $COUNTER -lt 1 ]; do
+
+      echo COUNTER $COUNTER
+
+      let COUNTER-=1
+
+done
+
 sleep 5
+
+echo ""
+
 echo -e  "\033[1;34m${PROMPT} teamviewer --daemon restart \033[0m"
 teamviewer --daemon restart
 echo ""
+
+COUNTER=10
+
+until [  $COUNTER -lt 1 ]; do
+
+      echo COUNTER $COUNTER
+
+      let COUNTER-=1
+
+done
+
 sleep 10
+
+echo ""
+
 echo -e  "\033[1;34m${PROMPT} teamviewer info \033[0m"
 teamviewer info
 echo ""
