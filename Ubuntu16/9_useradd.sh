4e6e200da44ba4f9d5e94c301c291cb679ec5480
milli-sewoo <pro3ax@gmail.com>
2018-10-24 14:11:42 +0900

    no message

--- Ubuntu16/9_useradd.sh
+++ Ubuntu16/9_useradd.sh
@@ -15,3 +15,17 @@ echo ${NEW_USER}
 echo ""
 echo -e  "\033[1;34m${PROMPT} adduser ${NEW_USER} \033[0m"
 adduser ${NEW_USER}
+echo ""
+
+echo " 관리자로 전환될 수 있도록, 새 사용자를 wheel 그룹에 포함. "
+echo ""
+echo -e  "\033[1;34m${PROMPT} grep ${NEW_USER} /etc/group \033[0m"
+grep ${NEW_USER} /etc/group
+echo ""
+echo -e  "\033[1;34m${PROMPT} usermod -G sudo ${NEW_USER} \033[0m"
+usermod -G sudo ${NEW_USER}
+echo ""
+echo -e  "\033[1;34m${PROMPT} grep ${NEW_USER} /etc/group \033[0m"
+grep ${NEW_USER} /etc/group
+
+echo " sonic유저 계정 로그아웃 후 dasan유저 계정으로 su - 확인 "
