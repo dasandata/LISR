1b054e1c2dac6bfc141b8b4c6c722fc3d3e456e5
milli-sewoo <pro3ax@gmail.com>
2018-10-24 13:35:46 +0900

    no message

--- Ubuntu16/8_ssh_firewall.sh
+++ Ubuntu16/8_ssh_firewall.sh
@@ -15,4 +15,24 @@ ufw status
 echo ""
 echo -e  "\033[1;34m${PROMPT} ufw enable \033[0m"
 ufw enable
-read Command may disrupt existing ssh connections. Proceed with operation (y|n)?
+read Command may disrupt existing ssh connections. Proceed with operation y|n?
+
+echo " 기본 정책을 차단 (deny) 로 변경한 후 필요한 포트 (또는 서비스) 만 허용. "
+
+echo ""
+
+echo -e  "\033[1;34m${PROMPT} ufw status \033[0m"
+ufw status
+echo ""
+echo -e  "\033[1;34m${PROMPT} ufw default deny \033[0m"
+ufw default deny
+echo ""
+echo -e  "\033[1;34m${PROMPT} ufw allow 22/tcp \033[0m"
+ufw allow 22/tcp
+echo ""
+echo -e  "\033[1;34m${PROMPT} ufw allow 7777/tcp \033[0m"
+ufw allow 7777/tcp
+echo ""
+echo -e  "\033[1;34m${PROMPT} ufw status numbered \033[0m"
+ufw status numbered
+echo ""
