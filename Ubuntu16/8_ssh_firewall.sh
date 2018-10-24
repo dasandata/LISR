cf8e4cc865c4f1092932f3e7afbfcc2ea7a2099c
milli-sewoo <pro3ax@gmail.com>
2018-10-24 13:51:52 +0900

    no message

--- Ubuntu16/8_ssh_firewall.sh
+++ Ubuntu16/8_ssh_firewall.sh
@@ -15,7 +15,7 @@ ufw status
 echo ""
 echo -e  "\033[1;34m${PROMPT} ufw enable \033[0m"
 ufw enable
-
+echo ""
 
 echo " 기본 정책을 차단 (deny) 로 변경한 후 필요한 포트 (또는 서비스) 만 허용. "
 
@@ -36,3 +36,34 @@ echo ""
 echo -e  "\033[1;34m${PROMPT} ufw status numbered \033[0m"
 ufw status numbered
 echo ""
+
+echo " sshd 설정. (기본 포트인 22번을 7777로 변경) "
+echo ""
+
+echo " Ubuntu16 sshd_config "
+echo ""
+echo -e  "\033[1;34m${PROMPT} grep 'Root\|Port' /etc/ssh/sshd_config \033[0m"
+grep 'Root\|Port' /etc/ssh/sshd_config
+echo ""
+echo -e  "\033[1;34m${PROMPT} perl -pi -e "s/Port 22/Port 7777/g" /etc/ssh/sshd_config \033[0m"
+perl -pi -e "s/Port 22/Port 7777/g" /etc/ssh/sshd_config
+echo ""
+echo -e  "\033[1;34m${PROMPT} perl -pi -e "s/PermitRootLogin prohibit-password/PermitRootLogin no/g" /etc/ssh/sshd_config \033[0m"
+perl -pi -e "s/PermitRootLogin prohibit-password/PermitRootLogin no/g" /etc/ssh/sshd_config
+echo ""
+echo -e  "\033[1;34m${PROMPT} grep 'Root\|Port' /etc/ssh/sshd_config \033[0m"
+grep 'Root\|Port' /etc/ssh/sshd_config
+
+echo ""
+echo ""
+echo " X11Forwarding 을 위해 AddressFamily inet 을 추가 합니다. "
+echo ""
+echo -e  "\033[1;34m${PROMPT} echo "AddressFamily inet" >> /etc/ssh/sshd_config \033[0m"
+echo "AddressFamily inet" >> /etc/ssh/sshd_config
+echo ""
+echo -e  "\033[1;34m${PROMPT} grep AddressFamily /etc/ssh/sshd_config \033[0m"
+grep AddressFamily /etc/ssh/sshd_config
+echo ""
+
+echo -e  "\033[1;34m${PROMPT} systemctl restart sshd \033[0m"
+systemctl restart sshd
