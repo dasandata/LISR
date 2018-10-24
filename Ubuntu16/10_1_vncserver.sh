285a16b48672dc27364c537295eeb139a8236585
milli-sewoo <pro3ax@gmail.com>
2018-10-24 16:43:27 +0900

    no message

--- Ubuntu16/10_1_vncserver.sh
+++ Ubuntu16/10_1_vncserver.sh
@@ -41,6 +41,7 @@ echo -e  "\033[1;34m${PROMPT} chmod +x .vnc/xstartup \033[0m"
 chmod +x .vnc/xstartup
 echo ""
 echo ""
+echo -e  "\033[1;34m${PROMPT} vnc4server \033[0m"
 vnc4server
 
 echo ""
@@ -51,6 +52,6 @@ echo ""
 echo -e  "\033[1;34m${PROMPT} vncserver -kill :1 \033[0m"
 vncserver -kill :1
 echo ""
-echo -e  "\033[1;34m${PROMPT} logout \033[0m"
-logout
+echo -e  "\033[1;34m${PROMPT} exit \033[0m"
+exit
 echo ""
