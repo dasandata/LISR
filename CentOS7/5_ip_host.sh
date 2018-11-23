8a210364a673e2830469939f8a22dca44977c2f3
milli-sewoo <pro3ax@gmail.com>
2018-11-23 15:27:09 +0900

    변수 컷 부분 수정. 대학 친구한테 물어봄 ; 근본이 늦어서 망함;
    수정 이유 : [root@dasan-test ~]# cat /etc/sysconfig/network-scripts/ifcfg- ens192
    cat: /etc/sysconfig/network-scripts/ifcfg-: No such file or directory
    cat: ens192: No such file or directory
    
    ifcfg- 192 한칸 띠어지고 들어가서 안되었던 문제

--- CentOS7/5_ip_host.sh
+++ CentOS7/5_ip_host.sh
@@ -10,8 +10,8 @@ echo -e  "\033[1;34m${PROMPT} ip a \033[0m"
 ip a
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} EXT_NIC=$(ip a | grep 'state UP' | cut -d ":" -f 2) \033[0m"
-EXT_NIC=$(ip a | grep 'state UP' | cut -d ":" -f 2)
+echo -e  "\033[1;34m${PROMPT} EXT_NIC=$(ip a | grep 'state UP' | cut -d ":" -f 2 |tr -d ' ') \033[0m"
+EXT_NIC=$(ip a | grep 'state UP' | cut -d ":" -f 2 |tr -d ' ')
 
 echo ""
 echo -e  "\033[1;34m${PROMPT} echo ${EXT_NIC} \033[0m"
@@ -22,5 +22,21 @@ echo -e  "\033[1;34m${PROMPT} ifconfig  ${EXT_NIC} \033[0m"
 ifconfig  ${EXT_NIC}
 
 echo ""
+echo -e  "\033[1;34m${PROMPT} sed -i '/IPV6/d' /etc/sysconfig/network-scripts/ifcfg-${EXT_NIC} \033[0m"
+sed -i '/IPV6/d' /etc/sysconfig/network-scripts/ifcfg-${EXT_NIC}
+
+echo ""
 echo -e  "\033[1;34m${PROMPT} cat /etc/sysconfig/network-scripts/ifcfg-${EXT_NIC} \033[0m"
 cat /etc/sysconfig/network-scripts/ifcfg-${EXT_NIC}
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} cat /etc/hostname \033[0m"
+cat /etc/hostname
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} cat /etc/resolv.conf \033[0m"
+cat /etc/resolv.conf
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} ping -c 4 google.com \033[0m"
+ping -c 4 google.com
