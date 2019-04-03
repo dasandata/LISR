a6cb278225b7bb253d69d295d10d5ef39035f670
cheekykite <xxue123@gmail.com>
2019-04-03 17:50:39 +0900

    온도 수집 방식 변경.

--- common/usr-local-sbin/dasan_temperature_check_to_log.sh
+++ common/usr-local-sbin/dasan_temperature_check_to_log.sh
@@ -8,13 +8,16 @@
 LOG='/var/log/Dasandata_Temperature.log'
 
 # Temperature Detect for Inlet
-AA=$(ipmitool sdr type Temperature)
-BB=$(echo ${AA} | cut -d "|" -f 5 | cut -d " " -f 2)
-echo $(date '+%Y-%m-%d %a %H:%M:%S')' Inlet Temp is ' ${BB} 'degrees C' >> $LOG
+#AA=$(ipmitool sdr type Temperature)
+#BB=$(echo ${AA} | cut -d "|" -f 5 | cut -d " " -f 2)
+#echo $(date '+%Y-%m-%d %a %H:%M:%S')' Inlet Temp is ' ${BB} 'degrees C' >> $LOG
 
 # Temperature Detect for Ambient
 #AA=$(ipmitool sdr type Temperature)
 #BB=$(echo ${AA} | cut -d "|" -f 21 | cut -d " " -f 2)
 #echo $(date '+%Y-%m-%d %a %H:%M:%S')' Ambient Temp is '${BB}' degrees C' >> $LOG
+
+CC=$(ipmitool sdr type Temperature | grep "Ambient\|Inlet" | awk '{print $1, $2, $10, $11, $12}')
+echo $(date '+%Y-%m-%d %a %H:%M:%S') ${CC} >> $LOG
 
 # End of File.
