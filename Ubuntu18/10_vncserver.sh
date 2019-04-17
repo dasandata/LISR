c4c21f7159e184028c4ebe68a9f74018a36c02de
milli-sewoo <pro3ax@gmail.com>
2019-04-17 11:19:49 +0900

    vnc  cp 명령 추가

--- Ubuntu18/10_vncserver.sh
+++ Ubuntu18/10_vncserver.sh
@@ -3,6 +3,11 @@
 PROMPT="[${USER}@${HOSTNAME%%.*} ${PWD/#$HOME/~}]#"
 
 echo " 원격 접속을 위한 VNC Server를 구성 합니다. "
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} cp LISR/Ubuntu18/10_1_vncserver.sh /home/sonic \033[0m"
+cp LISR/Ubuntu18/10_1_vncserver.sh /home/sonic
+
 echo ""
 echo -e  "\033[1;34m${PROMPT} dpkg --list | grep   vnc 현재 설치된 vnc 패키지 확인 \033[0m"
 dpkg --list | grep vnc
