e0a2a9da5cdb9b06f0d17638b3074d8e2a0b828b
milli-sewoo <pro3ax@gmail.com>
2019-07-11 21:30:41 +0900

    command add

--- CentOS7/2_utilinstall.sh
+++ CentOS7/2_utilinstall.sh
@@ -17,10 +17,10 @@ echo ""
 echo " 서버 기본 설정에 필요한 유틸리티들 설치 "
 echo ""
 
-echo -e  "\033[1;34m${PROMPT} yum install -y vim pciutils openssh mlocate nfs-utils rdate xauth firefox nautilus wget \033[0m"
+echo -e  "\033[1;34m${PROMPT} yum install -y vim pciutils openssh mlocate nfs-utils rdate xauth firefox nautilus wget ifconfig \033[0m"
 echo -e  "\033[1;34m${PROMPT} yum install -y tcsh tree lshw tmux git kernel-headers kernel-devel gcc make gcc-c++ \033[0m"
 echo -e  "\033[1;34m${PROMPT} yum install -y cmake python-devel ntfs-3g dstat perl perl-CPAN perl-core net-tools openssl-devel \033[0m"
-yum install -y vim pciutils openssh mlocate nfs-utils rdate xauth firefox nautilus wget >>  dasan_log_install_centos_default_util.txt 2>&1
+yum install -y vim pciutils openssh mlocate nfs-utils rdate xauth firefox nautilus wget ifconfig >>  dasan_log_install_centos_default_util.txt 2>&1
 yum install -y tcsh tree lshw tmux git kernel-headers kernel-devel gcc make gcc-c++ >>  dasan_log_install_centos_default_util.txt 2>&1
 yum install -y cmake python-devel ntfs-3g dstat perl perl-CPAN perl-core net-tools openssl-devel >>  dasan_log_install_centos_default_util.txt 2>&1
 
