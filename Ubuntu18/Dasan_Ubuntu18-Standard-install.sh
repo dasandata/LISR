b21769a6ed4d2f0dfec978c2133b205c032897a3
milli-sewoo <pro3ax@gmail.com>
2019-07-16 10:56:56 +0900

    history command file add

--- Ubuntu18/Dasan_Ubuntu18-Standard-install.sh
+++ Ubuntu18/Dasan_Ubuntu18-Standard-install.sh
@@ -343,8 +343,8 @@ echo ""
 echo -e  "\033[1;34m${PROMPT} ip a \033[0m"
 ip a
 echo ""
-echo -e  "\033[1;34m${PROMPT} EXT_NIC=$(ip a | grep 'state UP' | cut -d ":" -f 2) \033[0m"
-EXT_NIC=$(ip a | grep 'state UP' | cut -d ":" -f 2)
+echo -e  "\033[1;34m${PROMPT} EXT_NIC=$(ip a | grep 'state UP' | cut -d ":" -f 2 |tr -d ' ') \033[0m"
+EXT_NIC=$(ip a | grep 'state UP' | cut -d ":" -f 2 |tr -d ' ')
 echo ""
 echo -e  "\033[1;34m${PROMPT} echo ${EXT_NIC}  # 추출된 인터페이스 명이 맞는지 확인 필요. \033[0m"
 echo ${EXT_NIC}
