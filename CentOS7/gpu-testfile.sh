5dee9e54bfa9c6ed220c613f40a5db3dd12acf55
milli-sewoo <pro3ax@gmail.com>
2019-07-22 15:07:05 +0900

    add test file

--- CentOS7/gpu-testfile.sh
+++ CentOS7/gpu-testfile.sh
@@ -34,9 +34,109 @@ echo -e  "\033[1;34m${PROMPT} yum -y install openmotif*" >> /root/Dasan_command.
 yum -y install openmotif*
 
 echo ""
+echo ""
+
+echo -e  "\033[1;32m"==================== Cuda 10.0 설치 ===================="\033[0m"
+
+echo ""
 echo -e  "\033[1;34m${PROMPT} yum -y install yum -y install cuda-10-0" >> /root/Dasan_command.txt
 yum -y install cuda-10-0
 
+sleep 5
+
+echo "  Nvidia Driver 동작 확인. "
+
 echo ""
-echo -e  "\033[1;34m${PROMPT} nvidia-smi" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} ls /usr/local/ | grep cuda \033[0m" >> /root/Dasan_command.txt
+ls /usr/local/ | grep cuda
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} nvidia-smi -L \033[0m" >> /root/Dasan_command.txt
+nvidia-smi -L
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} nvidia-smi   \033[0m" >> /root/Dasan_command.txt
 nvidia-smi
+
+echo ""
+echo ""
+
+echo " Cuda 10.0 환경변수 Profile 에 추가 "
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} echo " "  >> /etc/profile \033[0m" >> /root/Dasan_command.txt
+echo " "  >> /etc/profile
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} echo " ADD Cuda 10.0 PATH "  >> /etc/profile \033[0m" >> /root/Dasan_command.txt
+echo " ### ADD Cuda 10.0 PATH "  >> /etc/profile
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} echo " export PATH=/usr/local/cuda-10.0/bin:/usr/local/cuda-10.0/include:\$PATH " >> /etc/profile \033[0m" >> /root/Dasan_command.txt
+echo " export PATH=/usr/local/cuda-10.0/bin:/usr/local/cuda-10.0/include:\$PATH " >> /etc/profile
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} echo " export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:/usr/local/cuda/extras/CUPTI/:\$LD_LIBRARY_PATH " >> /etc/profile \033[0m" >> /root/Dasan_command.txt
+echo " export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:/usr/local/cuda/extras/CUPTI/:\$LD_LIBRARY_PATH " >> /etc/profile
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} echo " export CUDA_HOME=/usr/local/cuda-10.0 " >> /etc/profile \033[0m" >> /root/Dasan_command.txt
+echo " export CUDA_HOME=/usr/local/cuda-10.0 " >> /etc/profile
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} echo " export CUDA_INC_DIR=/usr/local/cuda-10.0/include " >> /etc/profile \033[0m" >> /root/Dasan_command.txt
+echo " export CUDA_INC_DIR=/usr/local/cuda-10.0/include " >> /etc/profile
+
+echo ""
+echo ""
+
+echo " 추가 확인 "
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} tail -5  /etc/profile   \033[0m" >> /root/Dasan_command.txt
+tail  -5 /etc/profile
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} source /etc/profile   \033[0m" >> /root/Dasan_command.txt
+source /etc/profile
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} source .bashrc  \033[0m" >> /root/Dasan_command.txt
+source .bashrc
+
+
+sleep 10
+
+echo ""
+echo ""
+
+echo " Cuda 컴파일러 동작 확인. "
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} ls /usr/local/ | grep cuda  \033[0m" >> /root/Dasan_command.txt
+ls /usr/local/ | grep cuda
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} which nvcc \033[0m" >> /root/Dasan_command.txt
+which nvcc
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} nvcc -V \033[0m" >> /root/Dasan_command.txt
+nvcc -V
+
+echo ""
+echo ""
+
+echo " Cuda 10.0 샘플 컴파일 "
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} cp -r  /usr/local/cuda-10.0/samples/   ~/NVIDIA_CUDA-10.0_Samples \033[0m" >> /root/Dasan_command.txt
+cp -r  /usr/local/cuda-10.0/samples/   ~/NVIDIA_CUDA-10.0_Samples
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} cd ~/NVIDIA_CUDA-10.0_Samples \033[0m" >> /root/Dasan_command.txt
+cd ~/NVIDIA_CUDA-10.0_Samples
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} time make -j$(grep process /proc/cpuinfo | wc -l) \033[0m" >> /root/Dasan_command.txt
+time make -j$(grep process /proc/cpuinfo | wc -l)
