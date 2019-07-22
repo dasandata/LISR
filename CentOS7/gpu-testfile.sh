bc01173bf002f56fe667555b0b256ed7cd4587ef
milli-sewoo <pro3ax@gmail.com>
2019-07-22 14:48:23 +0900

    test add

--- CentOS7/gpu-testfile.sh
+++ CentOS7/gpu-testfile.sh
@@ -14,13 +14,29 @@ echo ""
 echo " Cuda-repo (Cuda 저장소) 설치 "
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} curl  -L -o  cuda-repo-rhel7-10.0.130-1.x86_64.rpm \033[0m" >> /root/Dasan_command.txt
-curl  -L -o  cuda-repo-rhel7-10.0.130-1.x86_64.rpm
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.0.130-1.x86_64.rpm \033[0m" >> /root/Dasan_command.txt
-http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.0.130-1.x86_64.rpm
+echo -e  "\033[1;34m${PROMPT} wget http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.0.130-1.x86_64.rpm \033[0m" >> /root/Dasan_command.txt
+wget http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.0.130-1.x86_64.rpm
 
 echo ""
 echo -e  "\033[1;34m${PROMPT} yum -y install cuda-repo-rhel7-10.0.130-1.x86_64.rpm" >> /root/Dasan_command.txt
-curl  -L -o  cuda-repo-rhel7-10.0.130-1.x86_64.rpm
+yum -y install cuda-repo-rhel7-10.0.130-1.x86_64.rpm
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} yum --disablerepo="*" --enablerepo="cuda" list available" >> /root/Dasan_command.txt
+yum --disablerepo="*" --enablerepo="cuda" list available
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} yum -y install  libXi-devel mesa-libGLU-devel libXmu-devel libX11-devel freeglut-devel libXm*" >> /root/Dasan_command.txt
+yum -y install  libXi-devel mesa-libGLU-devel libXmu-devel libX11-devel freeglut-devel libXm*
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} yum -y install openmotif*" >> /root/Dasan_command.txt
+yum -y install openmotif*
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} yum -y install yum -y install cuda-10-0" >> /root/Dasan_command.txt
+yum -y install cuda-10-0
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} nvidia-smi" >> /root/Dasan_command.txt
+nvidia-smi
