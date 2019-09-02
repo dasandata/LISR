b61e81ef76a20e2427042c57ee84d717e399a48b
milli-sewoo <pro3ax@gmail.com>
2019-09-02 14:28:16 +0900

    gpu test file add

---
+++ common/Dell-Server-Pcielfmmod-fanoffset.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Copyright (c) 2018 by Dasandata Corp.
+# run as a root
+
+ROOT_UID=0
+if [ "$UID" != "$ROOT_UID" ]; then
+  echo "Must be a root to run this script."
+  exit 1
+fi
+
+
+echo "# Custom + Off"
+racadm set system.PCIeSlotLFM.1.lfmmode 2  # custom
+racadm set system.PCIeSlotLFM.3.lfmmode 2  # custom
+racadm set system.PCIeSlotLFM.6.lfmmode 2  # custom
+racadm set system.PCIeSlotLFM.8.lfmmode 2  # custom
+
+racadm set system.ThermalSettings.FanSpeedOffset 255 # off
+
+
+bash /root/t640-gpubun-throttling-test.sh
+
+echo "# Auto + Off"
+racadm set system.PCIeSlotLFM.1.lfmmode 0  # auto
+racadm set system.PCIeSlotLFM.3.lfmmode 0  # auto
+racadm set system.PCIeSlotLFM.6.lfmmode 0  # auto
+racadm set system.PCIeSlotLFM.8.lfmmode 0  # auto
+
+racadm set system.ThermalSettings.FanSpeedOffset 255 # off
+
+echo "##### sleep 10min"
+sleep 1200
+
+bash /root/t640-gpubun-throttling-test.sh
+
+
+echo "# Custom +  max"
+racadm set system.PCIeSlotLFM.1.lfmmode 2  # custom
+racadm set system.PCIeSlotLFM.3.lfmmode 2  # custom
+racadm set system.PCIeSlotLFM.6.lfmmode 2  # custom
+racadm set system.PCIeSlotLFM.8.lfmmode 2  # custom
+
+racadm set system.ThermalSettings.FanSpeedOffset 3  # max
+
+echo "##### sleep 10min"
+sleep 1200
+
+bash /root/t640-gpubun-throttling-test.sh
+
+echo "######### Test Done"
---
+++ common/Dell-Server-gpuburn-throttling-test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright (c) 2018 by Dasandata Corp.
+# run as a root
+
+ROOT_UID=0
+if [ "$UID" != "$ROOT_UID" ]; then
+  echo "Must be a root to run this script."
+  exit 1
+fi
+
+FanOffset=$(racadm get system.ThermalSettings.FanSpeedOffset | grep FanSpeedOffset | cut -d '=' -f 2 )
+LFMMode=$(racadm get system.PCIeSlotLFM.1.LFMMode  | grep LFMMode | cut -d '=' -f 2)
+GPU=$(nvidia-smi -L | head -1 | awk '{print $3 $4 $5 $6 }')
+
+# export VAR
+export LOGFILENAME="/root/T640-${LFMMode}-${FanOffset}-${GPU}-4EA.txt"
+
+# gpu gurn
+cd ~
+git clone https://github.com/wilicc/gpu-burn  > /dev/null 2>&1
+cd gpu-burn
+git pull      > /dev/null 2>&1
+make          > /dev/null 2>&1
+nohup  ./gpu_burn $((60*11)) &
+cd ~
+
+# make log
+for I in $(seq 1 11)
+do  echo "" | tee -a $LOGFILENAME
+date    | tee -a $LOGFILENAME
+nvidia-smi  | grep C  | tee -a $LOGFILENAME
+nvidia-smi -q -d PERFORMANCE,clock  | grep "^GPU\|Thermal\|Graphics\|MHz" | grep -v "SM\|Memory\|Video\|N/A" | tee -a $LOGFILENAME
+sleep 60
+done
