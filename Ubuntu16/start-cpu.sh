b7eb540c289c21bf5b5261c5cfcc5081f09737a5
milli-sewoo <pro3ax@gmail.com>
2019-10-01 13:12:13 +0900

    Ubuntu 16.04 install file 1가지로 통합

--- Ubuntu16/start-cpu.sh
+++ Ubuntu16/start-cpu.sh
@@ -10,12 +10,12 @@ echo ""
 HISTFILE=~/.bash_history
 set -o history
 
-echo -e  "\033[1;34m${PROMPT} sed -i '13a bash /root/LISR/Ubuntu16/Dasan_Ubuntu16-Standard-install0.sh >> /root/dasan_install_log.txt' /etc/rc.local  \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} sed -i '13a bash /root/LISR/Ubuntu16/Dasan_Ubuntu16-Standard-install0.sh >> /root/dasan_install_log.txt' /etc/rc.local  \033[0m" >> /root/dasan_install_log.txt
 sed -i '13a bash /root/LISR/Ubuntu16/Dasan_Ubuntu16-Standard-install0.sh >> /root/dasan_install_log.txt' /etc/rc.local
 
-echo -e  "\033[1;34m${PROMPT} cat /etc/rc.local | sed -n 14p \033[0m" >> /root/Dasan_command.txt
-cat /etc/rc.local | sed -n 14p
+echo -e  "\033[1;34m${PROMPT} cat /etc/rc.local | sed -n 14p \033[0m" >> /root/dasan_install_log.txt
+cat /etc/rc.local | sed -n 14p >> /root/dasan_install_log.txt
 
 echo "  재부팅 "
-echo -e  "\033[1;34m${PROMPT} reboot \033[0m" >> /root/Dasan_command.txt
-reboot
+echo -e  "\033[1;34m${PROMPT} reboot \033[0m" >> /root/dasan_install_log.txt
+reboot >> /root/dasan_install_log.txt
--- Ubuntu16/start-gpu.sh
+++ Ubuntu16/start-gpu.sh
@@ -7,12 +7,12 @@ echo -e  "\033[1;32m"==================== rc-local file add ====================
 echo ""
 echo ""
 
-echo -e  "\033[1;34m${PROMPT} sed -i '13a bash /root/LISR/Ubuntu16/Dasan_Ubuntu16-Standard-install.sh >> /root/dasan_install_log.txt' /etc/rc.local  \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} sed -i '13a bash /root/LISR/Ubuntu16/Dasan_Ubuntu16-Standard-install.sh >> /root/dasan_install_log.txt' /etc/rc.local  \033[0m" >> /root/dasan_install_log.txt
 sed -i '13a bash /root/LISR/Ubuntu16/Dasan_Ubuntu16-Standard-install.sh >> /root/dasan_install_log.txt' /etc/rc.local
 
-echo -e  "\033[1;34m${PROMPT} cat /etc/rc.local | sed -n 14p \033[0m" >> /root/Dasan_command.txt
-cat /etc/rc.local | sed -n 14p
+echo -e  "\033[1;34m${PROMPT} cat /etc/rc.local | sed -n 14p \033[0m" >> /root/dasan_install_log.txt
+cat /etc/rc.local | sed -n 14p >> /root/dasan_install_log.txt
 
 echo "  재부팅 "
-echo -e  "\033[1;34m${PROMPT} reboot \033[0m" >> /root/Dasan_command.txt
-reboot
+echo -e  "\033[1;34m${PROMPT} reboot \033[0m" >> /root/dasan_install_log.txt
+reboot >> /root/dasan_install_log.txt
