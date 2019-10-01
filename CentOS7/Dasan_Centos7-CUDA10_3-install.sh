9698fbada4283b1bc7a8e6f5da414ed18c60d429
milli-sewoo <pro3ax@gmail.com>
2019-10-01 13:19:16 +0900

    Centos 7 install 파일 통합

--- CentOS7/Dasan_Centos7-CUDA10_3-install.sh
+++ CentOS7/Dasan_Centos7-CUDA10_3-install.sh
@@ -8,11 +8,11 @@ echo ""
 echo ""
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} ./NVIDIA_CUDA-10.0_Samples/bin/x86_64/linux/release/deviceQuery \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} ./NVIDIA_CUDA-10.0_Samples/bin/x86_64/linux/release/deviceQuery \033[0m" >> /root/dasan_install_log.txt
 ./NVIDIA_CUDA-10.0_Samples/bin/x86_64/linux/release/deviceQuery >> /root/dasan_install_log.txt 2>&1
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} ./NVIDIA_CUDA-10.0_Samples/bin/x86_64/linux/release/p2pBandwidthLatencyTest \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} ./NVIDIA_CUDA-10.0_Samples/bin/x86_64/linux/release/p2pBandwidthLatencyTest \033[0m" >> /root/dasan_install_log.txt
 ./NVIDIA_CUDA-10.0_Samples/bin/x86_64/linux/release/p2pBandwidthLatencyTest >> /root/dasan_install_log.txt 2>&1
 
 echo ""
@@ -21,27 +21,27 @@ echo ""
 echo -e  "\033[1;32m"==================== Tensorflow-gpu Test ===================="\033[0m"
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} git clone https://github.com/aymericdamien/TensorFlow-Examples.git \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} git clone https://github.com/aymericdamien/TensorFlow-Examples.git \033[0m" >> /root/dasan_install_log.txt
 git clone https://github.com/aymericdamien/TensorFlow-Examples.git >> /root/dasan_install_log.txt 2>&1
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} ls  TensorFlow-Examples \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} ls  TensorFlow-Examples \033[0m" >> /root/dasan_install_log.txt
 ls  TensorFlow-Examples
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} python        TensorFlow-Examples/examples/1_Introduction/helloworld.py \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} python        TensorFlow-Examples/examples/1_Introduction/helloworld.py \033[0m" >> /root/dasan_install_log.txt
 python        TensorFlow-Examples/examples/1_Introduction/helloworld.py >> /root/dasan_install_log.txt 2>&1
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} python        TensorFlow-Examples/examples/1_Introduction/basic_operations.py \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} python        TensorFlow-Examples/examples/1_Introduction/basic_operations.py \033[0m" >> /root/dasan_install_log.txt
 python        TensorFlow-Examples/examples/1_Introduction/basic_operations.py >> /root/dasan_install_log.txt 2>&1
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} python3      TensorFlow-Examples/examples/1_Introduction/helloworld.py \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} python3      TensorFlow-Examples/examples/1_Introduction/helloworld.py \033[0m" >> /root/dasan_install_log.txt
 python3      TensorFlow-Examples/examples/1_Introduction/helloworld.py >> /root/dasan_install_log.txt 2>&1
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} python3      TensorFlow-Examples/examples/1_Introduction/basic_operations.py \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} python3      TensorFlow-Examples/examples/1_Introduction/basic_operations.py \033[0m" >> /root/dasan_install_log.txt
 python3      TensorFlow-Examples/examples/1_Introduction/basic_operations.py >> /root/dasan_install_log.txt 2>&1
 
 echo ""
@@ -50,24 +50,24 @@ echo ""
 echo -e  "\033[1;32m"==================== GPU Burning Test ===================="\033[0m"
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} git clone https://github.com/wilicc/gpu-burn \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} git clone https://github.com/wilicc/gpu-burn \033[0m" >> /root/dasan_install_log.txt
 git clone https://github.com/wilicc/gpu-burn >> /root/dasan_install_log.txt 2>&1
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} cd gpu-burn \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} cd gpu-burn \033[0m" >> /root/dasan_install_log.txt
 cd gpu-burn >> /root/dasan_install_log.txt 2>&1
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} make \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} make \033[0m" >> /root/dasan_install_log.txt
 make >> /root/dasan_install_log.txt 2>&1
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} ./gpu_burn 60 \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} ./gpu_burn 60 \033[0m" >> /root/dasan_install_log.txt
 ./gpu_burn 60 >> /root/dasan_install_log.txt 2>&1
 
 echo ""
 echo ""
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} reboot  \033[0m" >> /root/Dasan_command.txt
+echo -e  "\033[1;34m${PROMPT} reboot  \033[0m" >> /root/dasan_install_log.txt
 reboot
