8443d84239d3b1828b4ac9635d50125f4f1ccfb8
milli-sewoo <pro3ax@gmail.com>
2019-10-04 13:52:50 +0900

    Test

--- 1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-1_CPU/start-cpu.sh
+++ 1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-1_CPU/start-cpu.sh
@@ -32,11 +32,11 @@ echo -e  "\033[1;32m"==================== Utility Install ===================="\
 echo ""
 
 echo "  기본 유틸 설치. "
-echo -e  "\033[1;34m${PROMPT} apt-get install -y vim nfs-common rdate xauth firefox gcc make net-tools \033[0m" >> /root/dasan_install_log.txt
+echo -e  "\033[1;34m${PROMPT} apt-get install -y vim nfs-common rdate xauth firefox gcc make net-tools ipmitool \033[0m" >> /root/dasan_install_log.txt
 echo -e  "\033[1;34m${PROMPT} apt-get install -y locate htop tmux git wget \033[0m" >> /root/dasan_install_log.txt
 echo -e  "\033[1;34m${PROMPT} apt-get install -y xfsprogs ntfs-3g aptitude lvm2 dstat curl \033[0m" >> /root/dasan_install_log.txt
 
-apt-get install -y vim nfs-common rdate xauth firefox gcc make locate htop tmux git wget net-tools >> /root/dasan_install_log.txt 2>&1
+apt-get install -y vim nfs-common rdate xauth firefox gcc make locate htop tmux git wget net-tools ipmitool >> /root/dasan_install_log.txt 2>&1
 apt-get install -y xfsprogs ntfs-3g aptitude lvm2 dstat curl >> /root/dasan_install_log.txt 2>&1
 
 tail -5 /root/dasan_install_log.txt
@@ -44,8 +44,8 @@ tail -5 /root/dasan_install_log.txt
 echo ""
 echo ""
 
-echo -e  "\033[1;34m${PROMPT} sed -i '1a bash /root/LISR/Ubuntu18/Dasan_Ubuntu18-Standard-install0.sh >> /root/dasan_install_log.txt' /etc/rc.local   \033[0m" >> /root/dasan_install_log.txt
-sed -i '1a bash /root/LISR/Ubuntu18/Dasan_Ubuntu18-Standard-install0.sh >> /root/dasan_install_log.txt' /etc/rc.local
+echo -e  "\033[1;34m${PROMPT} sed -i '1a bash /root/LISR/1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-1_CPU/Dasan_Ubuntu18-Standard-install0.sh >> /root/dasan_install_log.txt' /etc/rc.local   \033[0m" >> /root/dasan_install_log.txt
+sed -i '1a bash /root/LISR/1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-1_CPU/Dasan_Ubuntu18-Standard-install0.sh >> /root/dasan_install_log.txt' /etc/rc.local
 
 echo ""
 echo ""
