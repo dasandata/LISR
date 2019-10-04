7cc73ef22e946b518900cef734d1605b735cddf0
milli-sewoo <pro3ax@gmail.com>
2019-10-04 14:40:45 +0900

    Test

--- 2_Workstation_Desktop/2-1_Centos7/2-1-2_GPU/start-gpu.sh
+++ 2_Workstation_Desktop/2-1_Centos7/2-1-2_GPU/start-gpu.sh
@@ -8,8 +8,8 @@ echo ""
 echo -e  "\033[1;34m${PROMPT} chmod +x /etc/rc.d/rc.local  \033[0m" >> /root/dasan_install_log.txt
 chmod +x /etc/rc.d/rc.local
 
-echo -e  "\033[1;34m${PROMPT} sed -i '12a bash /root/LISR/CentOS7/Dasan_Centos7-yumupdate-install.sh >> /root/dasan_install_log.txt' /etc/rc.d/rc.local   \033[0m" >> /root/dasan_install_log.txt
-sed -i '12a bash /root/LISR/CentOS7/Dasan_Centos7-yumupdate-install.sh >> /root/dasan_install_log.txt' /etc/rc.d/rc.local
+echo -e  "\033[1;34m${PROMPT} sed -i '12a bash /root/LISR/2_Workstation_Desktop/2-1_Centos7/2-1-2_GPU/Dasan_Centos7-yumupdate-install.sh >> /root/dasan_install_log.txt' /etc/rc.d/rc.local   \033[0m" >> /root/dasan_install_log.txt
+sed -i '12a bash /root/LISR/2_Workstation_Desktop/2-1_Centos7/2-1-2_GPU/Dasan_Centos7-yumupdate-install.sh >> /root/dasan_install_log.txt' /etc/rc.d/rc.local
 
 echo -e  "\033[1;34m${PROMPT} cat /etc/rc.d/rc.local  | sed -n 13p \033[0m" >> /root/dasan_install_log.txt
 cat /etc/rc.d/rc.local  | sed -n 13p
