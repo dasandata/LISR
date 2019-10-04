e5f960d0885ebe2cf908176cbcf5dc96c8477983
milli-sewoo <pro3ax@gmail.com>
2019-10-04 19:44:21 +0900

    line error

--- 1_Server/1-1_Dell/1-1-2_Ubuntu16/1-1-2-1_CPU/Dasan_Ubuntu16-Standard-install0.sh
+++ 1_Server/1-1_Dell/1-1-2_Ubuntu16/1-1-2-1_CPU/Dasan_Ubuntu16-Standard-install0.sh
@@ -577,11 +577,11 @@ echo ""
 echo "  rc.local 등록 "
 echo ""
 
-echo -e  "\033[1;34m${PROMPT} sed -i '13s/Dasan_Ubuntu16-Standard-install0.sh/Dasan_Ubuntu16-dell-package-install.sh/g' /etc/rc.d/rc.local    \033[0m" >> /root/dasan_install_log.txt
-sed -i '13s/Dasan_Ubuntu16-Standard-install0.sh/Dasan_Ubuntu16-dell-package-install.sh/g' /etc/rc.d/rc.local
+echo -e  "\033[1;34m${PROMPT} sed -i '14s/Dasan_Ubuntu16-Standard-install0.sh/Dasan_Ubuntu16-dell-package-install.sh/g' /etc/rc.d/rc.local    \033[0m" >> /root/dasan_install_log.txt
+sed -i '14s/Dasan_Ubuntu16-Standard-install0.sh/Dasan_Ubuntu16-dell-package-install.sh/g' /etc/rc.d/rc.local
 
-echo -e  "\033[1;34m${PROMPT} cat /etc/rc.local | sed -n 13p \033[0m" >> /root/dasan_install_log.txt
-cat /etc/rc.local | sed -n 13p
+echo -e  "\033[1;34m${PROMPT} cat /etc/rc.local | sed -n 14p \033[0m" >> /root/dasan_install_log.txt
+cat /etc/rc.local | sed -n 14p
 
 echo "  재부팅 "
 echo -e  "\033[1;34m${PROMPT} reboot \033[0m" >> /root/dasan_install_log.txt
