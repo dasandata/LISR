6b391855144a4161bc43f611131a0d7bf436b98d
milli-sewoo <pro3ax@gmail.com>
2019-10-04 13:26:01 +0900

    Dell-Ubuntu16-GPU : add

--- 1_Server/1-1_Dell/1-1-2_Ubuntu16/1-1-2-2_GPU/start-gpu.sh
+++ 1_Server/1-1_Dell/1-1-2_Ubuntu16/1-1-2-2_GPU/start-gpu.sh
@@ -7,8 +7,8 @@ echo -e  "\033[1;32m"==================== rc-local file add ====================
 echo ""
 echo ""
 
-echo -e  "\033[1;34m${PROMPT} sed -i '13a bash /root/LISR/Ubuntu16/Dasan_Ubuntu16-Standard-install.sh >> /root/dasan_install_log.txt' /etc/rc.local  \033[0m" >> /root/dasan_install_log.txt
-sed -i '13a bash /root/LISR/Ubuntu16/Dasan_Ubuntu16-Standard-install.sh >> /root/dasan_install_log.txt' /etc/rc.local
+echo -e  "\033[1;34m${PROMPT} sed -i '13a bash /root/LISR/1_Server/1-1_Dell/1-1-2_Ubuntu16/1-1-2-2_GPU/Dasan_Ubuntu16-Standard-install.sh >> /root/dasan_install_log.txt' /etc/rc.local  \033[0m" >> /root/dasan_install_log.txt
+sed -i '13a bash /root/LISR/1_Server/1-1_Dell/1-1-2_Ubuntu16/1-1-2-2_GPU/Dasan_Ubuntu16-Standard-install.sh >> /root/dasan_install_log.txt' /etc/rc.local
 
 echo -e  "\033[1;34m${PROMPT} cat /etc/rc.local | sed -n 14p \033[0m" >> /root/dasan_install_log.txt
 cat /etc/rc.local | sed -n 14p >> /root/dasan_install_log.txt
