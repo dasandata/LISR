ec8c7495f26756f2b4a63132f99b98e67ddbb15c
milli-sewoo <pro3ax@gmail.com>
2019-10-04 14:47:22 +0900

    Test

--- 2_Workstation_Desktop/2-2_Ubuntu16/2-2-1_CPU/start-cpu.sh
+++ 2_Workstation_Desktop/2-2_Ubuntu16/2-2-1_CPU/start-cpu.sh
@@ -10,12 +10,12 @@ echo ""
 HISTFILE=~/.bash_history
 set -o history
 
-echo -e  "\033[1;34m${PROMPT} sed -i '13a bash /root/LISR/Ubuntu16/Dasan_Ubuntu16-Standard-install0.sh >> /root/dasan_install_log.txt' /etc/rc.local  \033[0m" >> /root/dasan_install_log.txt
-sed -i '13a bash /root/LISR/Ubuntu16/Dasan_Ubuntu16-Standard-install0.sh >> /root/dasan_install_log.txt' /etc/rc.local
+echo -e  "\033[1;34m${PROMPT} sed -i '13a bash /root/LISR/2_Workstation_Desktop/2-2_Ubuntu16/2-2-1_CPU/Dasan_Ubuntu16-Standard-install0.sh >> /root/dasan_install_log.txt' /etc/rc.local  \033[0m" >> /root/dasan_install_log.txt
+sed -i '13a bash /root/LISR/2_Workstation_Desktop/2-2_Ubuntu16/2-2-1_CPU/Dasan_Ubuntu16-Standard-install0.sh >> /root/dasan_install_log.txt' /etc/rc.local
 
 echo -e  "\033[1;34m${PROMPT} cat /etc/rc.local | sed -n 14p \033[0m" >> /root/dasan_install_log.txt
 cat /etc/rc.local | sed -n 14p
 
 echo "  재부팅 "
 echo -e  "\033[1;34m${PROMPT} reboot \033[0m" >> /root/dasan_install_log.txt
-reboot 
+reboot
