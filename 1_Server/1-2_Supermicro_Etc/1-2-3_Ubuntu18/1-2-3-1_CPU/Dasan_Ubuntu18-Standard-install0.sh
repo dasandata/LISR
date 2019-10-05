604b6a998587f3485787b8be5be04d16f43b6ca3
milli-sewoo <pro3ax@gmail.com>
2019-10-06 00:36:45 +0900

    Test

--- 1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-1_CPU/Dasan_Ubuntu18-Standard-install0.sh
+++ 1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-1_CPU/Dasan_Ubuntu18-Standard-install0.sh
@@ -484,6 +484,33 @@ sleep 10
 
 echo ""
 
+
+echo -e  "\033[1;32m"==================== RAID Controller Management MSM  ===================="\033[0m" >> /root/dasan_install_log.txt
+
+echo ""
+echo ""
+
+echo -e "Ubuntu18" >> /root/dasan_install_log.txt
+
+echo ""
+echo ""
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} cat /root/LISR/1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-1_CPU/Install_MSM_Ubuntu.sh  \033[0m" >> /root/dasan_install_log.txt
+cat /root/LISR/1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-1_CPU/Install_MSM_Ubuntu.sh
+
+echo ""
+echo -e  "\033[1;34m${PROMPT} bash /root/LISR/1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-1_CPU/Install_MSM_Ubuntu.sh \033[0m" >> /root/dasan_install_log.txt
+bash /root/LISR/1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-1_CPU/Install_MSM_Ubuntu.sh
+
+sleep 10
+
+echo ""
+echo ""
+
+
+
+
 echo -e  "\033[1;32m"==================== Grub Settings ===================="\033[0m"
 
 echo ""
