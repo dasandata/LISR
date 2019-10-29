02d3cc440a492c19111805a98ecd5a138083e220
milli-sewoo <pro3ax@gmail.com>
2019-10-29 20:18:28 +0900

    test

--- 2_Workstation_Desktop/2-3_Ubuntu18/2-3-2_GPU/start-gpu.sh
+++ 2_Workstation_Desktop/2-3_Ubuntu18/2-3-2_GPU/start-gpu.sh
@@ -2,59 +2,69 @@
 
 PROMPT="[${USER}@${HOSTNAME%%.*}]#"
 
-echo -e  "\033[1;32m"==================== rc-local file add ===================="\033[0m"
+echo ""
+echo ""
+
+
+echo " 패키지 로그 파일 추가 "
+touch /root/log.txt
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} printf '%s\n' '#!/bin/sh -e' 'exit 0' | tee -a /etc/rc.local \033[0m" >> /root/dasan_install_log.txt
+echo ""
+
+echo -e  "==================== rc-local file add ===================="
+
+echo ""
+echo -e  "${PROMPT} printf '%s\n' '#!/bin/sh -e' 'exit 0' | tee -a /etc/rc.local " >> /root/dasan_install_log.txt
 printf '%s\n' '#!/bin/sh -e' 'exit 0' | tee -a /etc/rc.local
 
 echo ""
 echo ""
 
-echo -e  "\033[1;34m${PROMPT} chmod +x /etc/rc.local  \033[0m" >> /root/dasan_install_log.txt
+echo -e  "${PROMPT} chmod +x /etc/rc.local  " >> /root/dasan_install_log.txt
 chmod +x /etc/rc.local
 
 echo ""
 echo ""
 
-echo -e  "\033[1;34m${PROMPT} systemctl restart rc-local.service  \033[0m" >> /root/dasan_install_log.txt
+echo -e  "${PROMPT} systemctl restart rc-local.service  " >> /root/dasan_install_log.txt
 systemctl restart rc-local.service
 
 echo ""
 echo ""
 
-echo -e  "\033[1;34m${PROMPT} systemctl status rc-local.service  \033[0m" >> /root/dasan_install_log.txt
+echo -e  "${PROMPT} systemctl status rc-local.service  " >> /root/dasan_install_log.txt
 systemctl status rc-local.service
 
 
-echo -e  "\033[1;32m"==================== Utility Install ===================="\033[0m"
+echo -e  "==================== Utility Install ===================="
 
 echo ""
 
 echo "  기본 유틸 설치. "
-echo -e  "\033[1;34m${PROMPT} apt-get install -y vim nfs-common rdate xauth firefox gcc make net-tools \033[0m" >> /root/dasan_install_log.txt
-echo -e  "\033[1;34m${PROMPT} apt-get install -y locate htop tmux git wget \033[0m" >> /root/dasan_install_log.txt
-echo -e  "\033[1;34m${PROMPT} apt-get install -y xfsprogs ntfs-3g aptitude lvm2 dstat curl \033[0m" >> /root/dasan_install_log.txt
+echo -e  "${PROMPT} apt-get install -y vim nfs-common rdate xauth firefox gcc make net-tools " >> /root/dasan_install_log.txt
+echo -e  "${PROMPT} apt-get install -y locate htop tmux git wget " >> /root/dasan_install_log.txt
+echo -e  "${PROMPT} apt-get install -y xfsprogs ntfs-3g aptitude lvm2 dstat curl " >> /root/dasan_install_log.txt
 
-apt-get install -y vim nfs-common rdate xauth firefox gcc make locate htop tmux git wget net-tools >> /root/dasan_install_log.txt 2>&1
-apt-get install -y xfsprogs ntfs-3g aptitude lvm2 dstat curl >> /root/dasan_install_log.txt 2>&1
+apt-get install -y vim nfs-common rdate xauth firefox gcc make locate htop tmux git wget net-tools >> /root/log.txt
+apt-get install -y xfsprogs ntfs-3g aptitude lvm2 dstat curl >> /root/log.txt
 
-tail -5 /root/dasan_install_log.txt
+tail -5 /root/log.txt
 
 echo ""
 echo ""
 
-echo -e  "\033[1;34m${PROMPT} sed -i '1a bash /root/LISR/2_Workstation_Desktop/2-3_Ubuntu18/2-3-2_GPU/Dasan_Ubuntu18-Standard-install.sh >> /root/dasan_install_log.txt' /etc/rc.local   \033[0m" >> /root/dasan_install_log.txt
+echo -e  "${PROMPT} sed -i '1a bash /root/LISR/2_Workstation_Desktop/2-3_Ubuntu18/2-3-2_GPU/Dasan_Ubuntu18-Standard-install.sh >> /root/dasan_install_log.txt' /etc/rc.local   " >> /root/dasan_install_log.txt
 sed -i '1a bash /root/LISR/2_Workstation_Desktop/2-3_Ubuntu18/2-3-2_GPU/Dasan_Ubuntu18-Standard-install.sh >> /root/dasan_install_log.txt' /etc/rc.local
 
 echo ""
 echo ""
 
-echo -e  "\033[1;34m${PROMPT} cat /etc/rc.local | sed -n 2p \033[0m" >> /root/dasan_install_log.txt
+echo -e  "${PROMPT} cat /etc/rc.local | sed -n 2p " >> /root/dasan_install_log.txt
 cat /etc/rc.local | sed -n 2p
 
 echo ""
 echo ""
 
-echo -e  "\033[1;34m${PROMPT} reboot \033[0m" >> /root/dasan_install_log.txt
+echo -e  "${PROMPT} reboot " >> /root/dasan_install_log.txt
 reboot
