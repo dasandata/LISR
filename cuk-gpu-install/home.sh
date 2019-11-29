bf3faa8ef9c8e0db2c25711dbff19205873e97d8
milli-sewoo <pro3ax@gmail.com>
2019-11-29 19:15:32 +0900

    start

--- cuk-gpu-install/home.sh
+++ cuk-gpu-install/home.sh
@@ -87,5 +87,5 @@ cp /root/LISR/cuk-gpu-install/copy_launcher.py /tmp/  >> /root/log.txt
 
 echo ""
 echo ""
-echo -e  "${PROMPT} cp /root/LISR/cuk-gpu-install/saved_launcher.txt\  /tmp/  " >> /root/dasan_install_log.txt
-cp /root/LISR/cuk-gpu-install/saved_launcher.txt\  /tmp/  >> /root/log.txt
+echo -e  "${PROMPT} cp /root/LISR/cuk-gpu-install/saved_launcher.txt /tmp/  " >> /root/dasan_install_log.txt
+cp /root/LISR/cuk-gpu-install/saved_launcher.txt /tmp/  >> /root/log.txt
