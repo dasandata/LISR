80eb89e1c4e07d9906fd0b118955ba058cbeb916
milli-sewoo <pro3ax@gmail.com>
2019-12-09 13:31:11 +0900

    ubuntu 16.04 desktop version repo cuda-cudnn test-1

--- 1_Server/1-2_Supermicro_Etc/1-2-2_Ubuntu16/1-2-2-2_GPU/Dasan_Ubuntu16-CUDA10_1-install.sh
+++ 1_Server/1-2_Supermicro_Etc/1-2-2_Ubuntu16/1-2-2-2_GPU/Dasan_Ubuntu16-CUDA10_1-install.sh
@@ -166,89 +166,28 @@ echo ""
 echo ""
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} mount -t nfs 192.168.0.5:file /mnt \033[0m" >> /root/dasan_install_log.txt
-mount -t nfs 192.168.0.5:file /mnt
+echo -e  "${PROMPT} wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.5.32-1+cuda10.0_amd64.deb " >> /root/dasan_install_log.txt
+wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.5.32-1+cuda10.0_amd64.deb
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} mkdir /root/cudnn7 \033[0m" >> /root/dasan_install_log.txt
-mkdir /root/cudnn7
+echo -e  "${PROMPT} wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.5.32-1+cuda10.0_amd64.deb " >> /root/dasan_install_log.txt
+wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.5.32-1+cuda10.0_amd64.deb
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} cp /mnt/2_windows\ 관련\ \(서울대\)/windows_cuda_file/cudnn-10.0-linux-x64-v7.* /root/cudnn7 \033[0m" >> /root/dasan_install_log.txt
-cp /mnt/2_windows\ 관련\ \(서울대\)/windows_cuda_file/cudnn-10.0-linux-x64-v7.* /root/cudnn7
-
-sleep 20
+echo -e  "${PROMPT} dpkg -i libcudnn7_7.6.5.32-1+cuda10.0_amd64.deb  " >> /root/dasan_install_log.txt
+dpkg -i libcudnn7_7.6.5.32-1+cuda10.0_amd64.deb
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} cd  /root/cudnn7 \033[0m" >> /root/dasan_install_log.txt
-cd  /root/cudnn7
+echo -e  "${PROMPT} dpkg -i libcudnn7-dev_7.6.5.32-1+cuda10.0_amd64.deb  " >> /root/dasan_install_log.txt
+dpkg -i libcudnn7-dev_7.6.5.32-1+cuda10.0_amd64.deb
 
 echo ""
-
-echo -e  "\033[1;34m${PROMPT} umount /mnt #마운트 해제 \033[0m" >> /root/dasan_install_log.txt
-umount /mnt #마운트 해제
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} pwd \033[0m" >> /root/dasan_install_log.txt
-pwd
+echo -e  "${PROMPT} apt-get update " >> /root/dasan_install_log.txt
+apt-get update >> /root/log.txt
 
 echo ""
-echo -e  "\033[1;34m${PROMPT} ls \033[0m" >> /root/dasan_install_log.txt
-ls
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} tar xvzf cudnn-10.0-linux-x64-v7.3.0.29.tgz \033[0m" >> /root/dasan_install_log.txt
-tar xvzf cudnn-10.0-linux-x64-v7.3.0.29.tgz
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} tar xvzf cudnn-10.0-linux-x64-v7.3.1.20.tgz \033[0m" >> /root/dasan_install_log.txt
-tar xvzf cudnn-10.0-linux-x64-v7.3.1.20.tgz
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} tar xvzf cudnn-10.0-linux-x64-v7.4.1.5.tgz \033[0m" >> /root/dasan_install_log.txt
-tar xvzf cudnn-10.0-linux-x64-v7.4.1.5.tgz
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} tar xvzf cudnn-10.0-linux-x64-v7.4.2.24.tgz \033[0m" >> /root/dasan_install_log.txt
-tar xvzf cudnn-10.0-linux-x64-v7.4.2.24.tgz
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} tar xvzf cudnn-10.0-linux-x64-v7.5.0.56.tgz \033[0m" >> /root/dasan_install_log.txt
-tar xvzf cudnn-10.0-linux-x64-v7.5.0.56.tgz
-
-sleep 20
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} ls -l cuda/include/ \033[0m" >> /root/dasan_install_log.txt
-ls -l cuda/include/
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} ls -l cuda/lib64/ \033[0m" >> /root/dasan_install_log.txt
-ls -l cuda/lib64/
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} chmod  a+r  cuda/include/* \033[0m" >> /root/dasan_install_log.txt
-chmod  a+r  cuda/include/*
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} chmod  a+r  cuda/lib64/* \033[0m" >> /root/dasan_install_log.txt
-chmod  a+r  cuda/lib64/*
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} cp  -rp  cuda/include/cudnn.h  /usr/local/cuda-10.0/include/ \033[0m" >> /root/dasan_install_log.txt
-cp  -rp  cuda/include/cudnn.h  /usr/local/cuda-10.0/include/
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} cp  -rp  cuda/lib64/libcudnn*  /usr/local/cuda-10.0/lib64/ \033[0m" >> /root/dasan_install_log.txt
-cp  -rp  cuda/lib64/libcudnn*  /usr/local/cuda-10.0/lib64/
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} ls -l /usr/local/cuda-10.0/lib64/libcudnn* \033[0m" >> /root/dasan_install_log.txt
-ls -l /usr/local/cuda-10.0/lib64/libcudnn*
-
-echo ""
-echo -e  "\033[1;34m${PROMPT} cd ~ \033[0m" >> /root/dasan_install_log.txt
-cd ~
+echo -e  "${PROMPT} apt-get install -y libcublas-dev " >> /root/dasan_install_log.txt
+apt-get install -y libcublas-dev >> /root/log.txt
 
 echo ""
 echo ""
