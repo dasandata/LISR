3ae131b6aaf90deb318b2da74dd74f9ebd8c9d18
milli-sewoo <pro3ax@gmail.com>
2020-09-25 15:03:55 +0900

    Supermicro 서버 경로 수정

--- 1_Server/1-2_Supermicro/1-2-3_Ubuntu18/software.sh
+++ 1_Server/1-2_Supermicro/1-2-3_Ubuntu18/software.sh
@@ -12,13 +12,13 @@ read WORD
 case $WORD in
     1 | cpu | CPU)
         echo start Ubuntu18 CPU software.
-        bash /root/LISR/1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-1_CPU/start-cpu.sh
+        bash /root/LISR/1_Server/1-2_Supermicro/1-2-3_Ubuntu18/1-2-3-1_CPU/start-cpu.sh
         ;;
     2 | gpu | GPU)
         echo start Ubuntu18 GPU software.
         echo " Installed application test file "
-        echo " bash /root/LISR/1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-2_GPU/Dasan_Ubuntu18-CUDA10_3-install.sh "
-        bash /root/LISR/1_Server/1-2_Supermicro_Etc/1-2-3_Ubuntu18/1-2-3-2_GPU/start-gpu.sh
+        echo " bash /root/LISR/1_Server/1-2_Supermicro/1-2-3_Ubuntu18/1-2-3-2_GPU/Dasan_Ubuntu18-CUDA10_3-install.sh "
+        bash /root/LISR/1_Server/1-2_Supermicro/1-2-3_Ubuntu18/1-2-3-2_GPU/start-gpu.sh
         ;;
     3 | exit | Exit)
         echo clear out.
