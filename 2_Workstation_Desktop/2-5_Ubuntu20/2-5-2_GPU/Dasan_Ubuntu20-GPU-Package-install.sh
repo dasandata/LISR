c1eef08cef48b370367586ac11a3e5b74543758a
milli-sewoo <pro3ax@gmail.com>
2020-11-16 17:33:47 +0900

    Ubuntu 20.04 / cudnn 추가

--- 2_Workstation_Desktop/2-5_Ubuntu20/2-5-2_GPU/Dasan_Ubuntu20-GPU-Package-install.sh
+++ 2_Workstation_Desktop/2-5_Ubuntu20/2-5-2_GPU/Dasan_Ubuntu20-GPU-Package-install.sh
@@ -156,29 +156,77 @@ time make -j$(grep process /proc/cpuinfo | wc -l) >> /root/log.txt
 echo ""
 echo ""
 
-## echo -e  "==================== Cudnn 7 install ===================="
+echo -e  "==================== Cudnn 8 install ===================="
 
-## echo ""
-## echo ""
+echo ""
+echo ""
 
-## echo ""
-## echo -e  "${PROMPT} apt-get install -y libcudnn7=7.6.5.32-1+cuda10.0 " >> /root/dasan_install_log.txt
-## apt-get install -y libcudnn7=7.6.5.32-1+cuda10.0
+echo ""
+echo -e  "${PROMPT} mount -t nfs 192.168.0.5:/file /mnt " >> /root/dasan_install_log.txt
+mount -t nfs 192.168.0.5:/file /mnt
 
-## echo ""
-## echo -e  "${PROMPT} apt-get install -y libcudnn7-dev=7.6.5.32-1+cuda10.0  " >> /root/dasan_install_log.txt
-## apt-get install -y libcudnn7-dev=7.6.5.32-1+cuda10.0
+echo ""
+echo -e  "${PROMPT} mkdir /root/cudnn " >> /root/dasan_install_log.txt
+mkdir /root/cudnn
 
-## echo ""
-## echo -e  "${PROMPT} apt-get update " >> /root/dasan_install_log.txt
-## apt-get update >> /root/log.txt
+echo ""
+echo -e  "${PROMPT} cp /mnt/1_OS/2_Linux/cudnn/cudnn-11.0-linux-x64-v8.0.2.39.tgz /root/cudnn/ " >> /root/dasan_install_log.txt
+cp /mnt/1_OS/2_Linux/cudnn/cudnn-11.0-linux-x64-v8.0.2.39.tgz /root/cudnn/ >> /root/log.txt
 
-## echo ""
-## echo -e  "${PROMPT} apt-get install -y libcublas-dev " >> /root/dasan_install_log.txt
-## apt-get install -y libcublas-dev >> /root/log.txt
+echo ""
+echo -e  "${PROMPT} umount /mnt " >> /root/dasan_install_log.txt
+umount /mnt >> /root/log.txt
 
-## echo ""
-## echo ""
+echo ""
+echo -e  "${PROMPT} cd /root/cudnn/ " >> /root/dasan_install_log.txt
+cd /root/cudnn/ >> /root/log.txt
+
+echo ""
+echo -e  "${PROMPT} pwd " >> /root/dasan_install_log.txt
+pwd >> /root/log.txt
+
+echo ""
+echo -e  "${PROMPT} ls -al " >> /root/dasan_install_log.txt
+ls -al >> /root/log.txt
+
+echo ""
+echo -e  "${PROMPT} tar xvzf cudnn-11.0-linux-x64-v8.0.2.39.tgz " >> /root/dasan_install_log.txt
+tar xvzf cudnn-11.0-linux-x64-v8.0.2.39.tgz >> /root/log.txt
+
+echo ""
+echo -e  "${PROMPT} ls -l cuda/include/* " >> /root/dasan_install_log.txt
+ls -l cuda/include/* >> /root/log.txt
+
+echo ""
+echo -e  "${PROMPT} ls -l cuda/lib64/* " >> /root/dasan_install_log.txt
+ls -l cuda/lib64/* >> /root/log.txt
+
+echo ""
+echo -e  "${PROMPT} chmod a+r cuda/include/* " >> /root/dasan_install_log.txt
+chmod a+r cuda/include/* >> /root/log.txt
+
+echo ""
+echo -e  "${PROMPT} chmod a+r cuda/lib64/* " >> /root/dasan_install_log.txt
+chmod a+r cuda/lib64/* >> /root/log.txt
+
+echo ""
+echo -e  "${PROMPT} cp -rp cuda/include/cudnn.h /usr/local/cuda-11.0/include/ " >> /root/dasan_install_log.txt
+cp -rp cuda/include/cudnn.h /usr/local/cuda-11.0/include/ >> /root/log.txt
+
+echo ""
+echo -e  "${PROMPT} cp -rp cuda/lib64/libcudnn* /usr/local/cuda-11.0/lib64/ " >> /root/dasan_install_log.txt
+cp -rp cuda/lib64/libcudnn* /usr/local/cuda-11.0/lib64/ >> /root/log.txt
+
+echo ""
+echo -e  "${PROMPT} ls -l /usr/local/cuda-11.0/lib64/libcudnn* " >> /root/dasan_install_log.txt
+ls -l /usr/local/cuda-11.0/lib64/libcudnn* >> /root/log.txt
+
+echo ""
+echo -e  "${PROMPT} cd " >> /root/dasan_install_log.txt
+cd >> /root/log.txt
+
+echo ""
+echo ""
 
 echo -e  "==================== Deep Learning Package Install python-PIP, tensorflow ===================="
 
