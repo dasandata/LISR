bcdb4ea214d45b12c31fa2f775fb25aa041f100a
uriver89 <uriver89@gmail.com>
2021-07-12 13:50:33 +0900

    Update Check_List.sh

--- LISR_LAS/Check_List.sh
+++ LISR_LAS/Check_List.sh
@@ -12,13 +12,16 @@ echo "http://www.dasandata.co.kr"  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
 
 # nouveau 끄기 및 grub 설정
-echo "Nouveau, GRUB Check"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Nouveau, GRUB Check ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
+echo "===== IPv6 disable,splash quiet remove check =====" | tee -a /root/LAS_Install_Log.txt
 cat /etc/default/grub | grep LINUX  >> /root/LAS_Install_Log.txt
+echo "===== blacklist nouveau check =====" | tee -a /root/LAS_Install_Log.txt
 cat /etc/modprobe.d/blacklist.conf | grep "blacklist nouveau"  >> /root/LAS_Install_Log.txt
+echo "===== options nouveau modeset=0 check =====" | tee -a /root/LAS_Install_Log.txt
 cat /etc/modprobe.d/blacklist.conf | grep "options nouveau modeset=0"  >> /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Nouveau, GRUB Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Nouveau, GRUB Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
@@ -26,21 +29,22 @@ sleep 1
 case $OSCHECK in 
     centos )
         echo ""  | tee -a /root/LAS_Install_Log.txt
-        echo "SELINUX Check"  | tee -a /root/LAS_Install_Log.txt
+        echo "===== SELINUX Check ====="  | tee -a /root/LAS_Install_Log.txt
         echo ""  | tee -a /root/LAS_Install_Log.txt
-        echo "getenforce"  | tee -a /root/LAS_Install_Log.txt
+        echo "===== selinux = disable ====="  | tee -a /root/LAS_Install_Log.txt
         getenforce  >> /root/LAS_Install_Log.txt
         cat /etc/selinux/config | grep "^SELINUX="  >> /root/LAS_Install_Log.txt
         echo ""  | tee -a /root/LAS_Install_Log.txt
-        echo "SELINUX Check Complete"  | tee -a /root/LAS_Install_Log.txt
+        echo "===== SELINUX Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
     ;;
     ubuntu )
         echo ""  | tee -a /root/LAS_Install_Log.txt
-        echo "Repository Check"  | tee -a /root/LAS_Install_Log.txt
+        echo "===== Repository Check ====="  | tee -a /root/LAS_Install_Log.txt
         echo ""  | tee -a /root/LAS_Install_Log.txt
+        echo "===== mirror.kakao.com ====="  | tee -a /root/LAS_Install_Log.txt
         cat /etc/apt/sources.list | grep -v "#\|^$" | head -3  >> /root/LAS_Install_Log.txt
         echo ""  | tee -a /root/LAS_Install_Log.txt
-        echo "Repository Check Complete"  | tee -a /root/LAS_Install_Log.txt
+        echo "===== Repository Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
     ;;
     *)
     ;;
@@ -50,31 +54,38 @@ sleep 1
 
 # 기본 패키지 설치
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Pacakage Install Check"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Pacakage Install Check ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
 case $OSCHECK in 
     centos )
+        echo "===== Kernel Version Check ====="  | tee -a /root/LAS_Install_Log.txt
         uname -r  >> /root/LAS_Install_Log.txt
+        echo "===== epel,htop install ? ====="  | tee -a /root/LAS_Install_Log.txt
         rpm -qa | grep htop  >> /root/LAS_Install_Log.txt
+        echo "===== kernel-headers version Check ====="  | tee -a /root/LAS_Install_Log.txt
         rpm -qa | grep kernel-headers  >> /root/LAS_Install_Log.txt
+        echo "===== kernel-devel version Check ====="  | tee -a /root/LAS_Install_Log.txt
         rpm -qa | grep kernel-devel  >> /root/LAS_Install_Log.txt
     ;;
     ubuntu )
+        echo "===== Kernel Version Check ====="  | tee -a /root/LAS_Install_Log.txt
         uname -r  >> /root/LAS_Install_Log.txt
+        echo "===== epel,htop install ? ====="  | tee -a /root/LAS_Install_Log.txt
         dpkg -l | grep htop  >> /root/LAS_Install_Log.txt
+        echo "===== kernel-headers Version Check ====="  | tee -a /root/LAS_Install_Log.txt
         dpkg -l | grep -i linux-headers  >> /root/LAS_Install_Log.txt
     ;;
     *)
     ;;
 esac
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Pacakage Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Pacakage Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
 # 프로필 설정
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Profile Check"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Profile Check ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
 case $OSCHECK in 
     centos )
@@ -87,85 +98,88 @@ case $OSCHECK in
     ;;
 esac
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Profile Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Profile Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
 # 서버 시간 동기화
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Time Check"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Time Check ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
+echo "===== Time ====="  | tee -a /root/LAS_Install_Log.txt
 date  >> /root/LAS_Install_Log.txt
+echo "===== H/W Time ====="  | tee -a /root/LAS_Install_Log.txt
 hwclock  >> /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Time Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Time Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
 # 파이썬 설치
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Python Install Check"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Python Version Check ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
 python3 -V  >> /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Python Install Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Python Version Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
 # 파이썬 패키지 설치
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Python Pacakage Install Check"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Python Pacakage Install Check ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
-pip list >> /root/LAS_Install_Log.txt
-pip list >> /root/LAS_Install_Log.txt
-pip3 list >> /root/LAS_Install_Log.txt
 pip3 list >> /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Python Pacakage Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Python Pacakage Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
 # 방화벽 설정
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Firewall Check"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Firewall Check ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
 case $OSCHECK in 
     centos )
+        echo "===== firewall service Check ====="  | tee -a /root/LAS_Install_Log.txt
         systemctl status firewalld  >> /root/LAS_Install_Log.txt
         echo ""  | tee -a /root/LAS_Install_Log.txt
+        echo "===== firewall port Check ====="  | tee -a /root/LAS_Install_Log.txt
         firewall-cmd --list-all  >> /root/LAS_Install_Log.txt
     ;;
     ubuntu )
+        echo "===== firewall service Check ====="  | tee -a /root/LAS_Install_Log.txt
         systemctl status ufw  >> /root/LAS_Install_Log.txt
         echo ""  | tee -a /root/LAS_Install_Log.txt
+        echo "===== firewall port Check ====="  | tee -a /root/LAS_Install_Log.txt
         ufw status  >> /root/LAS_Install_Log.txt
     ;;
     *)
     ;;
 esac
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Firewall Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Firewall Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
 # 사용자 생성 테스트
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "User Add Check Start"  | tee -a /root/LAS_Install_Log.txt
+echo "===== User Add Check Start ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
 cat /etc/passwd | grep dasan  >> /root/LAS_Install_Log.txt
 cat /etc/shadow | grep dasan  >> /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "User Add Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== User Add Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
 # H/W 사양 체크
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "H/W Check Start"  | tee -a /root/LAS_Install_Log.txt
+echo "===== H/W Check Start ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
 cat /root/hwcheck.txt  >> /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "H/W Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== H/W Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
@@ -174,7 +188,7 @@ nvidia-smi &> /dev/null
 if [ $? != 0 ]
 then
     echo ""  | tee -a /root/LAS_Install_Log.txt
-    echo "Check List Complete"  | tee -a /root/LAS_Install_Log.txt
+    echo "===== Check List Complete ====="  | tee -a /root/LAS_Install_Log.txt
     case $OSCHECK in 
         centos )
             sed -i '/root/d' /etc/rc.d/rc.local
@@ -192,7 +206,7 @@ then
     esac
 else
     echo ""  | tee -a /root/LAS_Install_Log.txt
-    echo "GPU Check List Start"  | tee -a /root/LAS_Install_Log.txt
+    echo "===== GPU Check List Start ====="  | tee -a /root/LAS_Install_Log.txt
 fi
 
 
@@ -200,7 +214,7 @@ sleep 1
 
 # CUDA,CUDNN Repo 설치
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "CUDA, CUDNN Repo Check Start"  | tee -a /root/LAS_Install_Log.txt
+echo "===== CUDA, CUDNN Repo Check Start ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
 case $OSCHECK in 
     centos )
@@ -214,26 +228,29 @@ case $OSCHECK in
     ;;
 esac
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "CUDA, CUDNN Repo Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== CUDA, CUDNN Repo Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
 # CUDA 설치 및 PATH 설정
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "CUDA Install Check Start"  | tee -a /root/LAS_Install_Log.txt
-echo ""  | tee -a /root/LAS_Install_Log.txt
+echo "===== Nvidia Driver Check ====="  | tee -a /root/LAS_Install_Log.txt
 nvidia-smi  >> /root/LAS_Install_Log.txt
+echo ""  | tee -a /root/LAS_Install_Log.txt
+echo "===== GPU Install Check ====="  | tee -a /root/LAS_Install_Log.txt
 nvidia-smi -L  >> /root/LAS_Install_Log.txt
+echo ""  | tee -a /root/LAS_Install_Log.txt
+echo "===== CUDA version Check ====="  | tee -a /root/LAS_Install_Log.txt
 which nvcc  >> /root/LAS_Install_Log.txt
 nvcc -V  >> /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "CUDA Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== CUDA Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
 # CUDNN 설치 및 PATH 설정
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "CUDNN Install Check Start"  | tee -a /root/LAS_Install_Log.txt
+echo "===== CUDNN Install Check Start ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
 case $OSCHECK in 
     centos )
@@ -246,30 +263,36 @@ case $OSCHECK in
     ;;
 esac
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "CUDNN Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== CUDNN Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
 # 딥러닝 패키지 설치(R,R Server, JupyterHub, Pycharm)
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Deep Learning Install Check Start"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Deep Learning Install Check Start ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
 case $OSCHECK in 
     centos )
+        echo "===== R,R-Server Check ====="  | tee -a /root/LAS_Install_Log.txt
         rpm -qa | grep rstudio  >> /root/LAS_Install_Log.txt
         rpm -qa | grep r-base  >> /root/LAS_Install_Log.txt
+        echo ""  | tee -a /root/LAS_Install_Log.txt
+        echo "===== JupyterHUB service Check ====="  | tee -a /root/LAS_Install_Log.txt
         systemctl enable jupyterhub.service  >> /root/LAS_Install_Log.txt
     ;;
     ubuntu )
+        echo "===== R,R-Server Check ====="  | tee -a /root/LAS_Install_Log.txt
         dpkg -l | grep rstudio  >> /root/LAS_Install_Log.txt
         dpkg -l | grep r-base  >> /root/LAS_Install_Log.txt
+        echo ""  | tee -a /root/LAS_Install_Log.txt
+        echo "===== JupyterHUB service Check ====="  | tee -a /root/LAS_Install_Log.txt
         systemctl enable jupyterhub.service  >> /root/LAS_Install_Log.txt
     ;;
     *)
     ;;
 esac
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "Deep Learning Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== Deep Learning Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
@@ -278,7 +301,7 @@ dmidecode | grep -i ipmi &> /dev/null
 if [ $? != 0 ]
 then
     echo ""  | tee -a /root/LAS_Install_Log.txt
-    echo "Check List Complete"  | tee -a /root/LAS_Install_Log.txt
+    echo "===== Check List Complete ====="  | tee -a /root/LAS_Install_Log.txt
     case $OSCHECK in 
         centos )
             sed -i '/root/d' /etc/rc.d/rc.local
@@ -296,18 +319,18 @@ then
     esac
 else
     echo ""  | tee -a /root/LAS_Install_Log.txt
-    echo "Server Check List Start"  | tee -a /root/LAS_Install_Log.txt
+    echo "===== Server Check List Start ====="  | tee -a /root/LAS_Install_Log.txt
 fi
 
 sleep 1
 
 # MegaRaid Storage Manager 설치
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "MSM Install Check Start"  | tee -a /root/LAS_Install_Log.txt
+echo "===== MSM Install Check Start ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
 ls /usr/local/MegaRAID\ Storage\ Manager/  >> /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "MSM Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== MSM Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
@@ -315,7 +338,7 @@ sleep 1
 if [ $VENDOR != "Dell" ]
 then
     echo ""  | tee -a /root/LAS_Install_Log.txt
-    echo "$VENDOR Server Check List Complete"  | tee -a /root/LAS_Install_Log.txt
+    echo "===== $VENDOR Server Check List Complete ====="  | tee -a /root/LAS_Install_Log.txt
         case $OSCHECK in 
         centos )
             sed -i '/root/d' /etc/rc.d/rc.local
@@ -333,23 +356,23 @@ then
     esac
 else
     echo ""  | tee -a /root/LAS_Install_Log.txt
-    echo "$VENDOR Server Check List Start"  | tee -a /root/LAS_Install_Log.txt
+    echo "===== $VENDOR Server Check List Start ====="  | tee -a /root/LAS_Install_Log.txt
 fi
 
 sleep 1
 
 # Dell OMSA 설치
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "OMSA Install Check Start"  | tee -a /root/LAS_Install_Log.txt
+echo "===== OMSA Install Check Start ====="  | tee -a /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
-systemctl status dataeng  >> /root/LAS_Install_Log.txt
+racadm get system.ServerInfo  >> /root/LAS_Install_Log.txt
 echo ""  | tee -a /root/LAS_Install_Log.txt
-echo "OMSA Check Complete"  | tee -a /root/LAS_Install_Log.txt
+echo "===== OMSA Check Complete ====="  | tee -a /root/LAS_Install_Log.txt
 
 sleep 1
 
 echo ""
-echo "Check List Complete"
+echo "===== Check List Complete ====="
 case $OSCHECK in 
     centos )
         sed -i '/root/d' /etc/rc.d/rc.local
