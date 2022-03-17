7e9dbf98e888a83c1afc85db6eb9aeda150445fd
cheekykite <xxue123@gmail.com>
2022-03-17 16:00:40 +0900

    LISR 에서 자동 스크립트 부분 제거.

--- LISR_LAS/r_jupyterhub.R
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env Rscript
-
-options(repos = c(CRAN = "https://cran.seoul.go.kr/"))
-
-install.packages(c('repr', 'IRdisplay', 'IRkernel'), type = 'source')
-
-IRkernel::installspec()
-
-IRkernel::installspec(user = FALSE)
